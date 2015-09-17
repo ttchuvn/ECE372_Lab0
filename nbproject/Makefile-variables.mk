346c67d1fb2e2283980829db71a0b968a4f71c50
ttchuvn <ttchu@email.arizona.edu>
2015-09-17 16:59:10 -0700

    led and main are updated

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=lab0.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/lab0.production.hex
+CND_ARTIFACT_NAME_default=ECE372_Lab0.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ECE372_Lab0.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=lab0.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lab0.tar
+CND_PACKAGE_NAME_default=ece372lab0.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ece372lab0.tar
--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/lab0.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=lab0.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=lab0/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/ECE372_Lab0.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=ECE372_Lab0.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=ece372lab0/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/lab0/bin
+makeDirectory ${TMPDIR}/ece372lab0/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/lab0.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/ece372lab0.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/lab0.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/ece372lab0.tar *
 checkReturnCode
 
 # Cleanup
